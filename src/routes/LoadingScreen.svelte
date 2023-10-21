5bef6c7dd720c923bc8c7e8f7d2226cd8a52a9df
Jonas List <mail@jonaslist.de>
2023-10-22 01:29:55 +0200

    Add tutorial

--- src/routes/LoadingScreen.svelte
+++ src/routes/LoadingScreen.svelte
@@ -1,1 +1,6 @@
-<div class="fixed inset-0 bg-black text-white flex items-center justify-center">loading</div>
+<div class="fixed inset-0 bg-black text-white flex items-center justify-center text-center">
+	Click around (click around)<br />
+	Click around (click around)<br />
+	Click around (click around)<br />
+	Click up, click up, and get down
+</div>
